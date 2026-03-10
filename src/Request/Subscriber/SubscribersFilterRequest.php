28fb6b9860b4178dbed3dfad2679690028867ed3
Tatevik <tatevikg1@gmail.com>
2026-03-10 16:32:52 +0400

    Remove sorting

--- src/Request/Subscriber/SubscribersFilterRequest.php
+++ src/Request/Subscriber/SubscribersFilterRequest.php
@@ -10,23 +10,17 @@ class SubscribersFilterRequest extends AbstractRequest
 {
     public ?bool $isConfirmed = null;
     public ?bool $isBlacklisted = null;
-    public ?string $sortBy = null;
-    public ?string $sortDirection = null;
     public ?string $findColumn = null;
     public ?string $findValue = null;
 
     public function __construct(
         ?bool $isConfirmed = null,
         ?bool $isBlacklisted = null,
-        ?string $sortBy = null,
-        ?string $sortDirection = null,
         ?string $findColumn = null,
         ?string $findValue = null
     ) {
         $this->isConfirmed = $isConfirmed;
         $this->isBlacklisted = $isBlacklisted;
-        $this->sortBy = $sortBy;
-        $this->sortDirection = $sortDirection;
         $this->findColumn = $findColumn;
         $this->findValue = $findValue;
     }
