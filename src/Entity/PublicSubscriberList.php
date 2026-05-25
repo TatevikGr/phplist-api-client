09acedb134d35ca20f7a3ede8d20a3d03de13678
Tatevik <tatevikg1@gmail.com>
2026-05-25 22:53:04 +0400

    Add: public subscriber list entity and retrieval method

---
+++ src/Entity/PublicSubscriberList.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpList\RestApiClient\Entity;
+
+use PhpList\RestApiClient\Response\AbstractResponse;
+
+/**
+ * Entity class for public subscriber list details.
+ */
+class PublicSubscriberList extends AbstractResponse
+{
+    /**
+     * @var int The list ID
+     */
+    public int $id;
+
+    /**
+     * @var string The list name
+     */
+    public string $name;
+
+    /**
+     * @var string|null The list description
+     */
+    public ?string $description;
+
+    public function __construct(array $data)
+    {
+        $this->id = isset($data['id']) ? (int)$data['id'] : 0;
+        $this->name = isset($data['name']) ? (string)$data['name'] : '';
+        $this->description = isset($data['description']) ? (string)$data['description'] : null;
+    }
+}
